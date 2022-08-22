526a4bd72e8b9a17966b402af1c238c4f55657f7
Aeonitis <Aeonitis@users.noreply.github.com>
2022-08-22 21:31:48 +0100

    Added Replication (Server to/with Clients)

--- Source/Alluka/MovingPlatform.h
+++ Source/Alluka/MovingPlatform.h
@@ -17,6 +17,8 @@ class ALLUKA_API AMovingPlatform : public AStaticMeshActor
 public:
 	AMovingPlatform();
 
+	virtual void BeginPlay() override;
+
 	virtual void Tick(float DeltaTime) override;
 
 	UPROPERTY(EditAnywhere)
